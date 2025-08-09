709942981fa0dc7bc74b893904055831106678d7
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-09 12:49:22 +0200

    fixed setup.sh

--- experiments/00001-logging/setup.sh
+++ experiments/00001-logging/setup.sh
@@ -1,4 +1,6 @@
 pip install uv
-uv init
-uv add numpy tqdm torch huggingface-hub matplotlib rich
+uv venv
+source .venv/bin/activate
+uv pip install numpy tqdm torch huggingface-hub matplotlib rich
+uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 --upgrade
 uv run data/cached_fineweb10B.py
