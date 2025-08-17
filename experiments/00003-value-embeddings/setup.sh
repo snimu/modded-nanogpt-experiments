4afd63928c50d3c7ec00b0a60b4a072a3ee49f0b
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-17 11:31:21 +0200

    00003: 11 & 12

--- experiments/00003-value-embeddings/setup.sh
+++ experiments/00003-value-embeddings/setup.sh
@@ -6,14 +6,16 @@ uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/
 uv run data/cached_fineweb10B.py
 
 cd runs
-torchrun --standalone --nproc_per_node=8 0-no-valemb-0.py
-torchrun --standalone --nproc_per_node=8 1-no-valemb-1.py
-torchrun --standalone --nproc_per_node=8 2-no-valemb-2.py
-torchrun --standalone --nproc_per_node=8 3-no-valemb-13.py
-torchrun --standalone --nproc_per_node=8 4-no-valemb-14.py
-torchrun --standalone --nproc_per_node=8 5-no-valemb-15.py
-torchrun --standalone --nproc_per_node=8 6-no-valemb-0-1.py
-torchrun --standalone --nproc_per_node=8 7-no-valemb-0-1-2.py
-torchrun --standalone --nproc_per_node=8 8-no-valemb-0-13.py
-torchrun --standalone --nproc_per_node=8 9-no-valemb-1-14.py
-torchrun --standalone --nproc_per_node=8 10-no-valemb-2-15.py
+torchrun --standalone --nproc_per_node=8 0-2025-08-13-no-valemb-0.py
+torchrun --standalone --nproc_per_node=8 1-2025-08-13-no-valemb-1.py
+torchrun --standalone --nproc_per_node=8 2-2025-08-13-no-valemb-2.py
+torchrun --standalone --nproc_per_node=8 3-2025-08-13-no-valemb-13.py
+torchrun --standalone --nproc_per_node=8 4-2025-08-13-no-valemb-14.py
+torchrun --standalone --nproc_per_node=8 5-2025-08-13-no-valemb-15.py
+torchrun --standalone --nproc_per_node=8 6-2025-08-13-no-valemb-0-1.py
+torchrun --standalone --nproc_per_node=8 7-2025-08-13-no-valemb-0-1-2.py
+torchrun --standalone --nproc_per_node=8 8-2025-08-13-no-valemb-0-13.py
+torchrun --standalone --nproc_per_node=8 9-2025-08-13-no-valemb-1-14.py
+torchrun --standalone --nproc_per_node=8 10-2025-08-13-no-valemb-2-15.py
+torchrun --standalone --nproc_per_node=8 11-2025-08-17-shared-valemb-012-131415.py
+torchrun --standalone --nproc_per_node=8 12-2025-08-17-shared-valemb-01-1415.py
