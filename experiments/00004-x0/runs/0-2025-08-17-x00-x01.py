daad6de1488628e4da1b68ca0d7aa7a70e1c89a2
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-20 18:17:32 +0200

    00004-x0: fix logs dir again

--- experiments/00004-x0/runs/0-2025-08-17-x00-x01.py
+++ experiments/00004-x0/runs/0-2025-08-17-x00-x01.py
@@ -388,7 +388,7 @@ master_process = (rank == 0) # this process will do logging, checkpointing etc.
 # begin logging
 if master_process:
     run_id_full = f"{run_id:03d}_{uuid.uuid4()}"
-    os.makedirs("../0-x00-x01/logs", exist_ok=True)
+    os.makedirs("../logs/0-x00-x01", exist_ok=True)
     logfile = f"../logs/0-x00-x01/{run_id_full}.txt"
     print(logfile)
 def print0(s, console=False):
