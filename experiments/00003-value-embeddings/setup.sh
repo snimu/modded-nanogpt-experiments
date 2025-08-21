cca6fcfa11f0f63dbddc4c19e3e622ab34f17d3f
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-21 12:15:42 +0200

    00003-value-embeddings: 14 & 15

--- experiments/00003-value-embeddings/setup.sh
+++ experiments/00003-value-embeddings/setup.sh
@@ -20,3 +20,5 @@ torchrun --standalone --nproc_per_node=8 10-2025-08-13-no-valemb-2-15.py
 torchrun --standalone --nproc_per_node=8 11-2025-08-17-shared-valemb-012-131415.py
 torchrun --standalone --nproc_per_node=8 12-2025-08-17-shared-valemb-01-1415.py
 torchrun --standalone --nproc_per_node=8 13-2025-08-20-baseline.py
+torchrun --standalone --nproc_per_node=8 14-2025-08-21-new-valemb-15.py
+torchrun --standalone --nproc_per_node=8 15-2025-08-21-new-valemb-3-15.py
