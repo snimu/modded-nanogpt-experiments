e89cc1299dfe1c10b9560f321cda811e06a6b326
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-21 12:06:57 +0200

    00004-x0: updated setup.sh with the new runs

--- experiments/00004-x0/setup.sh
+++ experiments/00004-x0/setup.sh
@@ -7,3 +7,6 @@ uv run data/cached_fineweb10B.py
 
 cd runs
 torchrun --standalone --nproc_per_node=8 0-2025-08-17-x00-x01.py
+torchrun --standalone --nproc_per_node=8 1-2025-08-20-x00-x01-x02.py
+torchrun --standalone --nproc_per_node=8 2-2025-08-21-x00-x01-x02-x03.py
+torchrun --standalone --nproc_per_node=8 3-2025-08-21-x00-x01-x02-x03-x04.py
