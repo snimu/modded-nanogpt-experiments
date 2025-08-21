07423eac1269ef7ec3b4f283864a384d59991cf6
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-21 12:45:39 +0200

    00002-rope-contributions: updated setup.sh

--- experiments/00002-rope-contributions/setup.sh
+++ experiments/00002-rope-contributions/setup.sh
@@ -4,3 +4,9 @@ source .venv/bin/activate
 uv pip install numpy tqdm torch huggingface-hub matplotlib rich
 uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 --upgrade
 uv run data/cached_fineweb10B.py
+
+cd runs
+torchrun --standalone --nproc_per_node=8 0-2025-08-09-measure-rope-contributions.py
+torchrun --standalone --nproc_per_node=8 1-2025-08-10-measure-rope-contributions-unnormalized.py
+torchrun --standalone --nproc_per_node=8 2-2025-08-21-measure-rope-via-theta.py
+torchrun --standalone --nproc_per_node=8 3-2025-08-21-measure-rope-via-theta-independent-qk.py
