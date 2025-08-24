6eac5e061e98b3b8a5aefe779abbc257d035f29b
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-24 21:05:43 +0200

    update setup.sh

--- experiments/00004-x0/setup.sh
+++ experiments/00004-x0/setup.sh
@@ -1,7 +1,7 @@
 pip install uv
 uv venv
 source .venv/bin/activate
-uv pip install numpy tqdm torch huggingface-hub matplotlib rich
+uv pip install numpy tqdm torch huggingface-hub matplotlib rich sentence_transformers tiktoken
 uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 --upgrade
 uv run data/cached_fineweb10B.py
 
@@ -10,3 +10,4 @@ torchrun --standalone --nproc_per_node=8 0-2025-08-17-x00-x01.py
 torchrun --standalone --nproc_per_node=8 1-2025-08-20-x00-x01-x02.py
 torchrun --standalone --nproc_per_node=8 2-2025-08-21-x00-x01-x02-x03.py
 torchrun --standalone --nproc_per_node=8 3-2025-08-21-x00-x01-x02-x03-x04.py
+torchrun --standalone --nproc_per_node=8 999-2025-08-20-baseline.py
