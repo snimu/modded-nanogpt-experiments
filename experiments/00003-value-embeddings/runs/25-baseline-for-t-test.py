a207bc6e54acdb3cf8a1877e0e4894012cf8a6c3
snimu <sebastian.nicolas.mueller@gmail.com>
2025-08-27 10:34:09 +0200

    fix logdir

--- experiments/00003-value-embeddings/runs/25-baseline-for-t-test.py
+++ experiments/00003-value-embeddings/runs/25-baseline-for-t-test.py
@@ -375,8 +375,8 @@ master_process = (rank == 0) # this process will do logging, checkpointing etc.
 # begin logging
 if master_process:
     run_id_full = f"{run_id:03d}_{uuid.uuid4()}"
-    os.makedirs("logs", exist_ok=True)
-    logfile = f"../logs/{run_id_full}.txt"
+    os.makedirs("../logs/25-baseline-for-t-test", exist_ok=True)
+    logfile = f"../logs/25-baseline-for-t-test/{run_id_full}.txt"
     print(logfile)
 def print0(s, console=False):
     if master_process:
