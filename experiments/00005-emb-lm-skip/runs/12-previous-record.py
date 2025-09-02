5b02eabf0b1855c7fa0391b46b677491a4542cb5
snimu <sebastian.nicolas.mueller@gmail.com>
2025-09-02 19:14:45 +0200

    00005-emb-lm-skip: fix logdir 12

--- experiments/00005-emb-lm-skip/runs/12-previous-record.py
+++ experiments/00005-emb-lm-skip/runs/12-previous-record.py
@@ -385,8 +385,8 @@ master_process = (rank == 0) # this process will do logging, checkpointing etc.
 # begin logging
 if master_process:
     run_id_full = f"{run_id:03d}_{uuid.uuid4()}"
-    os.makedirs("../logs/999-record", exist_ok=True)
-    logfile = f"../logs/999-record/{run_id_full}.txt"
+    os.makedirs("../logs/14-previous-record-x00-x01-x02", exist_ok=True)
+    logfile = f"../logs/14-previous-record-x00-x01-x02/{run_id_full}.txt"
     print(logfile)
 def print0(s, console=False):
     if master_process:
