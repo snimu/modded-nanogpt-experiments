8adc7ec72b0b2acb1b354c03ad2b2e711d798e71
snimu <sebastian.nicolas.mueller@gmail.com>
2025-09-30 07:06:50 +0200

    00005-emb-lm-skip: fix run_id in 5002

--- experiments/00005-emb-lm-skip/runs/5002-add-normed-skip11-to-x-out-record.py
+++ experiments/00005-emb-lm-skip/runs/5002-add-normed-skip11-to-x-out-record.py
@@ -381,7 +381,7 @@ master_process = (rank == 0) # this process will do logging, checkpointing etc.
 if master_process:
     run_id_full = f"{run_id:03d}_{uuid.uuid4()}"
     os.makedirs("../logs/5002-add-normed-skip11-to-x-out-record", exist_ok=True)
-    logfile = "../logs/5002-add-normed-skip11-to-x-out-record/{run_id_full}.txt"
+    logfile = f"../logs/5002-add-normed-skip11-to-x-out-record/{run_id_full}.txt"
     print(logfile)
 def print0(s, console=False):
     if master_process:
