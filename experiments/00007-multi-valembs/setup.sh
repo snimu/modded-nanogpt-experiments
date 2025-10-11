7829b0883511bd980adc85dcd89387236e7ac228
snimu <sebastian.nicolas.mueller@gmail.com>
2025-10-11 11:42:17 +0200

    updated setup.sh

--- experiments/00007-multi-valembs/setup.sh
+++ experiments/00007-multi-valembs/setup.sh
@@ -1,9 +1,45 @@
 pip install uv
 uv venv
 source .venv/bin/activate
-uv pip install numpy tqdm torch huggingface-hub matplotlib rich
+uv pip install numpy tqdm torch huggingface-hub matplotlib rich scipy
 uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 --upgrade
 uv run data/cached_fineweb10B.py
 
 cd runs
-torchrun --standalone --nproc_per_node=8 0-template.py
+torchrun --standalone --nproc-per-node=8 00001-multi-emb-via-projection.py
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00002-multi-small-emb-up-projected.py -d 2
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00002-multi-small-emb-up-projected.py -d 4
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00002-multi-small-emb-up-projected.py -d 8
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00002-multi-small-emb-up-projected.py -d 16
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00003-multi-emb-via-projection-relu.py
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00004-multi-small-emb-up-projected-relu.py -d 2
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00004-multi-small-emb-up-projected-relu.py -d 4
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00004-multi-small-emb-up-projected-relu.py -d 8
+cd .. && python plot_results.py --print-final-stats --path=logs
+cd runs
+torchrun --standalone --nproc-per-node=8 00004-multi-small-emb-up-projected-relu.py -d 16
+cd .. && python plot_results.py --print-final-stats --path=logs
+
+for ((num_ve=4; num_ve>0; num_ve--)); do
+    for ((num_embs_per_ve=1; num_embs_per_ve<6; num_embs_per_ve++)); do
+    cd runs
+    torchrun --standalone --nproc-per-node=8 00000-extra-embs.py --num-ve=$num_ve --num-embs-per-ve=$num_embs_per_ve
+    cd .. && python plot_results.py --print-final-stats --path=logs
+    done
+done
