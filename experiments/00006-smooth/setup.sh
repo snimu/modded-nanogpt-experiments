41118153f4e48eaf09cab50f163ca97279e90d58
snimu <sebastian.nicolas.mueller@gmail.com>
2025-10-31 14:22:33 +0100

    00006-smooth: plotting and setup

--- experiments/00006-smooth/setup.sh
+++ experiments/00006-smooth/setup.sh
@@ -5,6 +5,30 @@ uv pip install numpy tqdm torch huggingface-hub matplotlib rich scipy torchinfo 
 uv pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 --upgrade
 uv run data/cached_fineweb10B.py
 
+for ((idx=0; idx<10; idx++)); do
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0014-mtp-with-trafo-no-gate.py
+    cd .. && python plot_results.py --print-final-stats --path=logs  
+done
+
+for ((idx=0; idx<10; idx++)); do
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0000-baseline.py
+    cd .. && python plot_results.py --print-final-stats --path=logs  
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0009-mtp-same-layer-with-trafo.py -l=7
+    cd .. && python plot_results.py --print-final-stats --path=logs
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0010-mtp-with-trafo.py -l=7
+    cd .. && python plot_results.py --print-final-stats --path=logs
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0013-mtp-no-gate.py
+    cd .. && python plot_results.py --print-final-stats --path=logs
+    cd runs
+    torchrun --standalone --nproc-per-node=8 0004-mtp.py -l=7
+    cd .. && python plot_results.py --print-final-stats --path=logs  
+done
+
 for ((idx=0; idx<5; idx++)); do
     cd runs
     torchrun --standalone --nproc-per-node=8 0013-mtp-no-gate.py -l=7
