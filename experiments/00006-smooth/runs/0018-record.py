454eda5c7b16c8023aba0456ee993c7c030e99f5
snimu <sebastian.nicolas.mueller@gmail.com>
2025-11-01 20:57:39 +0100

    00006-smooth: increase steps to 5530 (from 5520)

--- experiments/00006-smooth/runs/0018-record.py
+++ experiments/00006-smooth/runs/0018-record.py
@@ -643,7 +643,7 @@ class Hyperparameters:
     train_seq_len = 64 * 1024  # FlexAttention sequence length
     val_seq_len = 4 * 64 * 1024  # FlexAttention sequence length for validation
     # optimization
-    num_iterations = 5520  # number of iterations to run
+    num_iterations = 5530  # number of iterations to run
     cooldown_frac = 0.7  # fraction of training spent cooling down the learning rate
     final_lr_scale = 0.01
     # architecture
